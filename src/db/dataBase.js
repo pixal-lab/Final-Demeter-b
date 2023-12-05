33601f847c30a6609daa2efb8b6879c08834d831
DevRaki <samuelriosaristizabal@gmail.com>
2023-12-05 05:03:04 -0500

    Update dataBase.js

--- src/db/dataBase.js
+++ src/db/dataBase.js
@@ -3,11 +3,10 @@ import { Sequelize } from 'sequelize';
 
 const dbConfig = {
   host: process.env.BD_HOST || "localhost",
-  port: process.env.BD_POR || "3306",
+  port: process.env.BD_PORT || "3306",
   username: process.env.BD_USER || 'root',
   password: process.env.BD_PASSWORD || "",
   database: process.env.BD_NAME || "demeterfinal",
-  url : "demeterfinal",
   dialect: 'mysql' 
   
 }
