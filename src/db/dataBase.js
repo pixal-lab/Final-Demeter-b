2842d9b08c2666ee8c586f9bf037f0fad5c90bd6
DevRaki <samuelriosaristizabal@gmail.com>
2023-12-05 02:29:03 -0500

    a
    a

--- src/db/dataBase.js
+++ src/db/dataBase.js
@@ -3,7 +3,7 @@ import { Sequelize } from 'sequelize';
 export const sequelize = new Sequelize(
   'demeterfinal',
   'root',
-  'Elefante1.',
+  '',
   {
     host: 'localhost',
     dialect: 'mysql' 
