57b19d21e731d5597cf56da46ccc47850c307698
DevRaki <samuelriosaristizabal@gmail.com>
2023-12-05 03:08:03 -0500

    a
    a

--- src/db/dataBase.js
+++ src/db/dataBase.js
@@ -1,11 +1,18 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize(
-  'demeterfinal',
-  'root',
-  '',
-  {
-    host: 'localhost',
-    dialect: 'mysql' 
-  }
-);
+// Verificar si la variable de entorno RAILWAY_DATABASE_URL está presente
+const railwayDatabaseURL = process.env.RAILWAY_DATABASE_URL;
+
+// Configuración predeterminada si RAILWAY_DATABASE_URL no está presente
+const localDatabaseConfig = {
+  database: 'demeterfinal',
+  username: 'root',
+  password: '',
+  host: 'localhost',
+  dialect: 'mysql'
+};
+
+// Utilizar la configuración de Railway si está presente, de lo contrario, utilizar la configuración local
+const databaseConfig = railwayDatabaseURL ? { url: railwayDatabaseURL } : localDatabaseConfig;
+
+export const sequelize = new Sequelize(databaseConfig);
