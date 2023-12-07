7a7259a419aee485807eac0ebac8348acb31700a
DevRaki <samuelriosaristizabal@gmail.com>
2023-12-07 06:36:25 -0500

    Revert "Update app.js"
    This reverts commit c1376a53ebb208b18c4cb631a95f17c330ddfe08.

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ import productRoutes from './routes/product.routes.js';
 const app = express();
 
 const corsOptions = {
-    origin: 'https://demeter-final-back-production.up.railway.app', // Ajusta esto según tus necesidades
+    origin: '*', // Ajusta esto según tus necesidades
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     //credentials: true, // Habilitar el intercambio de cookies (si es necesario)
     optionsSuccessStatus: 204,
