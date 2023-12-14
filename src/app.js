611184f7b9c0e01447979a2167656136a9eb31bf
DevRaki <samuelriosaristizabal@gmail.com>
2023-12-14 16:21:46 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ import productRoutes from './routes/product.routes.js';
 const app = express();
 
 const corsOptions = {
-    origin: ["http://localhost:3000", "https://demeter-final-back-production.up.railway.app"], // Ajusta esto según tus necesidades
+    origin: 'https://final-demeter-production.up.railway.app', // Ajusta esto según tus necesidades
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Habilitar el intercambio de cookies (si es necesario)
     optionsSuccessStatus: 204,
