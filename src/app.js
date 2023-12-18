e567d46cb462e5277f9b76dc07278a2353be13ad
Pablo Ahumada <pablo.ahumada1@mail.udp.cl>
2023-12-17 22:37:04 -0300

    .

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ import productRoutes from './routes/product.routes.js';
 const app = express();
 
 const corsOptions = {
-    origin: ["https://final-demeter-production-dd6a.up.railway.app/","https://final-demeter-b-production.up.railway.app/"], // Ajusta esto según tus necesidades
+    origin: ["https://final-demeter-production-dd6a.up.railway.app/", /\.railway\.app$/], // Ajusta esto según tus necesidades
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Habilitar el intercambio de cookies (si es necesario)
     optionsSuccessStatus: 204,
